1d943e8bc940851142b9d35e33d87c4437e5f2f0
KerolosAssad <143562491+KerolosAssad@users.noreply.github.com>
2025-05-03 22:45:38 -0400

    Update oauth_config.py (Working Google Login)

--- Backend/oauth_config.py
+++ Backend/oauth_config.py
@@ -10,5 +10,6 @@ def init_oauth(app):
         client_id=os.getenv("GOOGLE_CLIENT_ID"),
         client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
         server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-        client_kwargs={'scope': 'openid email profile'}
+        client_kwargs={'scope': 'openid email profile'},
+        redirect_uri='https://menumate-backend.onrender.com/auth/google/callback' #This needs to be changed, let me know to what link
     )
